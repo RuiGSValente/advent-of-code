836a9bea60738cbc6b9d788e200015b30bcb4937
Rui Valente <rui.valente@carpe.io>
2020-12-01 17:23:25 +0000

    day 1 advent of code

--- src/main/scala/day1/ReportRepair.scala
+++ src/main/scala/day1/ReportRepair.scala
@@ -6,14 +6,13 @@ object ReportRepair extends App {
 
   val listOfNumbers: Seq[Int] = readFromResources("01_12_2020.txt").map(_.toInt)
 
-  println(
-    searchSumBetweenTwo(listOfNumbers, value = 2020)
-      .map(tuple => tuple._1 * tuple._2)
-  )
+  searchSumBetweenTwo(listOfNumbers, value = 2020)
+    .map(tuple => println("Result part 1 day 1 " + tuple._1 * tuple._2))
 
-  println(
-    searchSumBetweenThree(listOfNumbers, value = 2020)
-      .map(triple => triple._1 * triple._2 * triple._3)
-  )
+  searchSumBetweenThree(listOfNumbers, value = 2020)
+    .map(
+      triple =>
+        println("Result part 2 day 1 " + triple._1 * triple._2 * triple._3)
+    )
 
 }
