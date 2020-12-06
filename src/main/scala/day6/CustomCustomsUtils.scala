a4e3ad268c23e08b42125c43282bf8b59462840a
Rui Valente <rui.valente@carpe.io>
2020-12-06 23:31:09 +0000

    day 6 advent of code

--- src/main/scala/day6/CustomCustomsUtils.scala
+++ src/main/scala/day6/CustomCustomsUtils.scala
@@ -3,7 +3,7 @@ package day6
 object CustomCustomsUtils {
 
   def countDistinctCharacters(seqStr: Seq[String]): Int = {
-    seqStr.map(_.distinct.count(x => !x.isWhitespace)).sum
+    seqStr.map(_.distinct.count(_.isLetter)).sum
   }
 
   def countCommonCharactersByGroup(seqStr: Seq[String]): Int = {
