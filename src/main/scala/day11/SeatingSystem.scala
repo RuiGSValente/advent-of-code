129b6f38dce5a37cb461e8953997d5839c325cf4
Rui Valente <rui.valente@carpe.io>
2020-12-12 03:01:34 +0000

    day 11 advent of code

--- src/main/scala/day11/SeatingSystem.scala
+++ src/main/scala/day11/SeatingSystem.scala
@@ -9,12 +9,18 @@ object SeatingSystem extends App {
     readFromResources("11_12_2020.txt").map(_.toArray).toArray
 
   val resultOccupiedSeats: Int =
-    adjacentSeatCalculator(listOfPath).map(_.toSeq.count(_ == '#')).toSeq.sum
+    visibleSeatCalculator(listOfPath, 4, firstPart = true)
+      .map(_.toSeq.count(_ == '#'))
+      .toSeq
+      .sum
 
   println("Result part 1 day 10: " + resultOccupiedSeats)
 
   val visibleOccupiedSeats =
-    visibleSeatCalculator(listOfPath).map(_.toSeq.count(_ == '#')).toSeq.sum
+    visibleSeatCalculator(listOfPath, 5, firstPart = false)
+      .map(_.toSeq.count(_ == '#'))
+      .toSeq
+      .sum
 
   println("Result part 2 day 10: " + visibleOccupiedSeats)
 
--- src/main/scala/day11/SeatingSystemUtils.scala
+++ src/main/scala/day11/SeatingSystemUtils.scala
@@ -1,27 +1,49 @@
 package day11
 
-import day11.test.{columnsRange, currentMap, i, j, rowRange}
-
 object SeatingSystemUtils {
 
-  //Part 1
-  def adjacentSeq(i: Int, j: Int, currentMap: Array[Array[Char]]): Seq[Char] = {
-    Seq(
-      (i - 1, j - 1),
-      (i - 1, j),
-      (i - 1, j + 1),
-      (i, j - 1),
-      (i, j + 1),
-      (i + 1, j - 1),
-      (i + 1, j),
-      (i + 1, j + 1)
-    ).flatMap {
-      case (row, col) => currentMap.lift(row).flatMap(_.lift(col))
+  //Part 1 and 2 Auxiliary
+  def visiblesSeatCalculation(i: Int,
+                              j: Int,
+                              currentMap: Array[Array[Char]],
+                              firsPart: Boolean): Int = {
+    @scala.annotation.tailrec
+    def searchUntil(iPosition: Int,
+                    jPosition: Int,
+                    iIncrement: Int,
+                    jIncrement: Int): Option[Char] = {
+      currentMap
+        .lift(iPosition + iIncrement)
+        .flatMap(_.lift(jPosition + jIncrement)) match {
+        case Some(seatChar) =>
+          if (seatChar == '.') {
+            searchUntil(
+              iPosition + iIncrement,
+              jPosition + jIncrement,
+              iIncrement,
+              jIncrement
+            )
+          } else Some(seatChar)
+        case None => None
+      }
     }
+
+    Seq((0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1))
+      .flatMap {
+        case (iIncrement, jIncrement) =>
+          if (firsPart)
+            currentMap.lift(i + iIncrement).flatMap(_.lift(j + jIncrement))
+          else
+            searchUntil(i, j, iIncrement, jIncrement)
+      }
+      .count(_ == '#')
   }
 
+  //Main part 1 and 2
   @scala.annotation.tailrec
-  def adjacentSeatCalculator(oldMap: Array[Array[Char]]): Array[Array[Char]] = {
+  def visibleSeatCalculator(oldMap: Array[Array[Char]],
+                            thresholdFreeSeat: Int,
+                            firstPart: Boolean): Array[Array[Char]] = {
 
     val newMap: Array[Array[Char]] = oldMap.map(_.clone)
 
@@ -29,16 +51,14 @@ object SeatingSystemUtils {
       for (j <- oldMap.head.indices) {
         oldMap(i)(j) match {
           case 'L' =>
-            if (adjacentSeq(i, j, oldMap).count(_ == '#') == 0)
+            if (visiblesSeatCalculation(i, j, oldMap, firstPart) == 0)
               newMap(i).update(j, '#')
-            //Maybe this can be deleted
             else 'L'
           case '#' =>
-            if (adjacentSeq(i, j, oldMap).count(_ == '#') >= 4)
+            if (visiblesSeatCalculation(i, j, oldMap, firstPart) >= thresholdFreeSeat)
               newMap(i).update(j, 'L')
-            //Maybe this can be deleted
             else '#'
-          case x => newMap(i)(j) = x
+          case otherChar => newMap(i)(j) = otherChar
         }
       }
     }
@@ -46,81 +66,7 @@ object SeatingSystemUtils {
     if (newMap.zip(oldMap).forall { case (i, j) => i sameElements j }) {
       newMap
     } else {
-      adjacentSeatCalculator(newMap)
+      visibleSeatCalculator(newMap, thresholdFreeSeat, firstPart)
     }
   }
-
-  //Part 2
-
-  //def searchUntil(i: Int, j: Int, iIncrement, jIncrement, currentMap) = {
-
-  //}
-  def visibleSeq(i: Int, j: Int, currentMap: Array[Array[Char]]): Int = {
-
-    val columnsRange: Seq[Int] = currentMap.head.indices
-
-    val rowRange: Seq[Int] = currentMap.indices
-
-    val horizontalO = (1 to j).map(x => currentMap(i)(j - x))
-
-    val horizontalE = (j + 1 to columnsRange.last).map(x => currentMap(i)(x))
-
-    val verticalN = (1 to i).map(x => currentMap(i - x)(j))
-
-    val verticalS = (i + 1 to rowRange.last).map(x => currentMap(x)(j))
-
-    val diagonalNO =
-      (1 to Seq(i, j).min).map(pos => currentMap(i - pos)(j - pos))
-
-    val diagonalSE = (1 to Seq(rowRange.last - i, columnsRange.last - j).min)
-      .map(pos => currentMap(i + pos)(j + pos))
-
-    val diagonalSO = (1 to Seq(rowRange.last - i, j).min)
-      .map(pos => currentMap(i + pos)(j - pos))
-
-    val diagonalNE = (1 to Seq(i, columnsRange.last - j).min)
-      .map(pos => currentMap(i - pos)(j + pos))
-
-    Seq(
-      horizontalE,
-      horizontalO,
-      verticalN,
-      verticalS,
-      diagonalNE,
-      diagonalSO,
-      diagonalSE,
-      diagonalNO
-    ).flatMap(_.find(_ != '.')).count(_ == '#')
-
-  }
-
-  def visibleSeatCalculator(oldMap: Array[Array[Char]]): Array[Array[Char]] = {
-
-    val newMap: Array[Array[Char]] = oldMap.map(_.clone)
-
-    for (i <- oldMap.indices) {
-      for (j <- oldMap.head.indices) {
-        oldMap(i)(j) match {
-          case 'L' =>
-            if (visibleSeq(i, j, oldMap) == 0)
-              newMap(i).update(j, '#')
-            //Maybe this can be deleted
-            else 'L'
-          case '#' =>
-            if (visibleSeq(i, j, oldMap) >= 5)
-              newMap(i).update(j, 'L')
-            //Maybe this can be deleted
-            else '#'
-          case x => newMap(i)(j) = x
-        }
-      }
-    }
-
-    if (newMap.zip(oldMap).forall { case (i, j) => i sameElements j }) {
-      newMap
-    } else {
-      visibleSeatCalculator(newMap)
-    }
-  }
-
 }
