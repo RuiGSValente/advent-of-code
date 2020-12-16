b18cb931704984aa3d445deea2fe94ceeb3b963b
Rui Valente <rui.valente@carpe.io>
2020-12-16 02:42:26 +0000

    day 15 advent of code

---
+++ src/main/scala/day15/RambunctiousRecitationUtils.scala
@@ -0,0 +1,30 @@
+package day15
+
+object RambunctiousRecitationUtils {
+
+  @scala.annotation.tailrec
+  def memoryGameMap(mapNumbers: Map[Int, Int],
+                    lastValue: Int,
+                    length: Int,
+                    threshold: Int): Int = {
+
+    if (length == threshold) lastValue
+    else {
+
+      val lastWithoutLast: Option[Int] = mapNumbers.get(lastValue)
+
+      val newMap: Map[Int, Int] = mapNumbers ++ Map(lastValue -> length)
+
+      if (lastWithoutLast.isEmpty)
+        memoryGameMap(newMap, 0, length + 1, threshold)
+      else
+        memoryGameMap(
+          newMap,
+          length - lastWithoutLast.get,
+          length + 1,
+          threshold
+        )
+    }
+  }
+
+}
